25dada1b954afc0c5efe7e95ec7f4c059f2fac55
MrBunnyDc <wonejo0109@gmail.com>
2024-09-23 10:26:24 -0400

    More changes

--- src/index.js
+++ src/index.js
@@ -9,44 +9,52 @@ class Status {
     static Cancelled = new Status({ color: 0xeeb10e, friendlyName: 'Cancelled' });
     static Timedout = new Status({ color: 0x7290c1, friendlyName: 'Timed-Out' });
 
-    constructor ({ color, friendlyName }) {
+    constructor({ color, friendlyName }) {
         this.color = color;
         this.friendlyName = friendlyName;
     }
 }
 
-const StartedStatus = Status.Success;
+const StartedStatus = Status.Started;
 const SuccessStatus = Status.Success;
-const FailedStatus = Status.Success;
-const CancelledStatus = Status.Success;
-const TimestoutStatus = Status.Success;
+const FailedStatus = Status.Failed;
+const CancelledStatus = Status.Cancelled;
+const TimedoutStatus = Status.Timedout;
 
-module.exports = async function run () {
+module.exports = async function run() {
     try {
         const octo = getOctokit(process.env.GITHUB_TOKEN);
-        const lastCommit = await octo.rest.repos.getCommit({ ...context.repo, ref: context.sha });
+
+        const lastCommit = await octo.rest.repos.getCommit({
+            ...context.repo,
+            ref: context.sha
+        });
+        
+        const commitStatus = await octo.rest.repos.getCombinedStatusForRef({
+            ...context.repo,
+            ref: context.sha
+        });
+        const status = getByStatus(commitStatus.data.state);
 
         const fields = [];
-        fields.push({ name: 'Build Branch', value: context.payload.ref?.toString().replace("refs/heads/", ""), inline: true});
-        fields.push({ name: `Commit Sender`, value: lastCommit.data.author.name, inline:  true });
+        fields.push({ name: 'Build Branch', value: context.payload.ref?.toString().replace("refs/heads/", ""), inline: true });
+        fields.push({ name: `Commit Sender`, value: lastCommit.data.author.name, inline: true });
 
-        const status = getByStatus(lastCommit.status);
-        
         const embed = {
             title: `Build ${status.friendlyName} | [${context.repo.repo}](https://github.com/${context.repo.owner}/${context.repo.repo})`,
             type: 'rich',
             description: `\`\`\`${lastCommit.data.commit.message}\`\`\``,
             color: status.color,
-            url: lastCommit.data.url,
-            timestamp: new Date().toISOString (),
+            url: lastCommit.data.html_url,
+            timestamp: new Date().toISOString(),
             fields: fields,
             author: {
                 name: `${lastCommit.data.author.name}`,
-                url: `${lastCommit.data.author.url}`,
+                url: `${lastCommit.data.author.html_url}`,
                 icon_url: `${lastCommit.data.author.avatar_url}`
             }
         };
-        
+
         const json = {
             username: getInput("webhook_name"),
             avatar_url: getInput("webhook_avatar"),
@@ -55,16 +63,16 @@ module.exports = async function run () {
 
         console.log(`Post Body: ${JSON.stringify(json)}`);
 
-        const respose = await axios.post(getInput("webhook_url"), json, {
+        const response = await axios.post(getInput("webhook_url"), json, {
             headers: {
                 "Content-Type": "application/json"
             }
         });
 
-        console.log(`Webhook respose: ${respose.status} - ${JSON.stringify(response.data)}`);
-    } catch ( error ) {
+        console.log(`Webhook response: ${response.status} - ${JSON.stringify(response.data)}`);
+    } catch (error) {
         console.error(`Failed to send webhook: ${error}`);
-        console.error(`Error details: ${error.respose ? JSON.stringify(error.response.data) : error.message}`);
+        console.error(`Error details: ${error.response ? JSON.stringify(error.response.data) : error.message}`);
         setFailed(error.message);
     }
 }
@@ -83,7 +91,7 @@ function getByStatus(status) {
             return CancelledStatus;
         case 'timedout':
         case 'timeout':
-            return TimestoutStatus;
+            return TimedoutStatus;
         default:
             return StartedStatus;
     }
